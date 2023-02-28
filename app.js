762579385e8369040910396736d97172f99bfacf
ftrbnd <giosalas25@gmail.com>
2023-02-28 12:23:29 -0800

    Write displayBooks function and style layouts

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-let myLibrary = [
-    new Book('The Perks of Being a Wallflower', 'Stephen Chbosky', 256, 'read'),
-    new Book('Almond', 'Sohn Won-pyung', 272, 'read'),
-    new Book('The Invisible Life of Addie LaRue', 'V. E. Schwab', 448, 'not read yet')
-];
-
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
+        this.author = `by ${author}`;
+        this.pages = `${pages} pages`;
+        this.read = read ? 'read' : 'not read yet';
     }
 
     info() {
@@ -17,14 +11,38 @@ class Book {
     }
 }
 
+let myLibrary = [
+    new Book('The Perks of Being a Wallflower', 'Stephen Chbosky', 256, true),
+    new Book('Almond', 'Sohn Won-pyung', 272, true),
+    new Book('The Invisible Life of Addie LaRue', 'V. E. Schwab', 448, false)
+];
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
 function displayBooks() {
-    const books = [];
+    const library = document.querySelector('div#library');
     for (const book of myLibrary) {
-        books.push(book.info());
+        const bookCard = document.createElement('div');
+
+        const title = document.createElement('h1');
+        title.textContent = book.title;
+        const author = document.createElement('h2');
+        author.textContent = book.author;
+        const pages = document.createElement('p');
+        pages.innerHTML = book.pages;
+        const read = document.createElement('p');
+        read.innerHTML = book.read;
+
+        bookCard.appendChild(title);
+        bookCard.appendChild(author)
+        bookCard.appendChild(pages);
+        bookCard.appendChild(read);
+        bookCard.classList.add('card');
+
+        library.appendChild(bookCard);
     }
-    return books;
 }
+
+displayBooks();
